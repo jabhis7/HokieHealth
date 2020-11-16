576bdef5db8d2b60470bc5d58d9b0b4ce31610a6
abhishek <jabhis7@vt.edu>
2020-11-15 21:14:04 -0500

    Dynamic form

--- angular-frontend/src/app/authenticate-user/authenticate-user.component.ts
+++ angular-frontend/src/app/authenticate-user/authenticate-user.component.ts
@@ -1,58 +1,67 @@
-import { Component } from '@angular/core';
-import { first } from 'rxjs/operators';
-
-
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../_services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {NotificationService} from '../_services/notification.service';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 
 @Component({ templateUrl: 'authenticate-user.component.html' ,
   styleUrls: ['authenticate-user.component.css']})
-export class AuthenticateUserComponent {
+export class AuthenticateUserComponent implements OnInit {
   loginForm: FormGroup;
   loading = false;
   submitted = false;
+  returnUrl: string;
   error = '';
-  username: string;
-  password: string;
 
   constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService,
-    private notif: NotificationService,
-    private formBuilder: FormBuilder
+    private notification: NotificationService
   ) {
-
+    // redirect to home if already logged in
+    if (this.authService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
   }
 
-  ngOninit() {
-    this.loginForm = this.formBuilder.group({username:['', Validators.required], password:['',Validators.required]});
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
-  login() {
+
+  // convenience getter for easy access to form fields//346947  286166
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
     this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.loading = true;
-    this.authService.login(this.username, this.password)
+    this.authService.login(this.f.username.value, this.f.password.value)
       .pipe(first())
       .subscribe(
-        user => {
-          this.router.navigate(['']);
-
-          this.notif.showNotif(user.username + ' has logged in!', 'confirmation');
+        data => {
+          this.router.navigate([this.returnUrl]);
         },
         error => {
           this.error = error;
           this.loading = false;
           // show a snackbar to user
-          this.notif.showNotif(this.error, 'dismiss');
-          console.log('Error', error);
+          this.notification.showNotif(this.error, 'undo');
         });
   }
-
-  get f() {
-    return this.loginForm.controls;
-  }
 }
-
-
